439a955572dfa96c2731121da1a7b84c319235ed
Md Mahmudunnobe <mahmud.nobe@edu.minerva.edu>
2022-10-19 20:43:51 -0400

    add app.run if it is main

--- app.py
+++ app.py
@@ -74,3 +74,7 @@ def stats(start=None, end=None):
         filter(Measurement.date <= end).all()
     temps = list(np.ravel(results))
     return jsonify(temps)
+
+if __name__ == "__main__":
+    app.run(debug = True)
+    
